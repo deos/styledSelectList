173dc632b505b1fb116be9a9817271e41ff4b741
deos <deos.dev@gmail.com>
2011-07-30 20:39:06 +0200

    extended list and removed select

--- source/styledSelectList.js
+++ source/styledSelectList.js
@@ -6,24 +6,45 @@ var styledSelectList = new Class({
     },
     
     initialize: function(element, options){
-        this.element = document.id(element);
-        this.setOptions(options);  
+        element = document.id(element);
+        this.setOptions(options);
+        
+        var self = this;
+        
+        var selectedItem = element.getElements('option')[element.selectedIndex];
+        
+        this.selectedOptionValueElement = new Element('input', {
+            'typ': 'text',
+            'value': selectedItem.getAttribute('value'),
+            'class': element.get('class'),
+            'styles': {
+                'display': 'none'
+            }
+        }).cloneEvents(element).inject(element, 'after');
        
-        this.wrapper = new Element('div', { 'class': this.options.wrapperClass }).inject(this.element, 'after');
+        this.wrapper = new Element('div', {
+            'class': this.options.wrapperClass
+        }).inject(element, 'after');
         
-        var selectedItem = this.element.getElements('option')[this.element.selectedIndex];
-        
-        new Element('div', { html: selectedItem.get('html') }).inject(this.wrapper);
+        this.selectedOptionElement = new Element('div', { html: selectedItem.get('html') }).inject(this.wrapper);
         
         var ul = new Element('ul').inject(this.wrapper);
 
-        this.element.getChildren('option').each(function(opt){
+        element.getChildren('option').each(function(opt){
             new Element('li', { 'html': opt.get('html') })
-                .store('optionValue', opt.get('value'))
+                .store('optionValue', opt.getAttribute('value') || '')
+                .addEvent('click', function(e){
+                    if(e){ e.preventDefault(); }
+
+                    if(self.selectedOptionValueElement.get('value')!=this.retrieve('optionValue')){
+                        self.selectedOptionElement.set('html', this.get('html'));
+                        self.selectedOptionValueElement.set('value', this.retrieve('optionValue')).fireEvent('change');
+                    }
+                })
                 .inject(ul);
         });
-            
-        new Element('input', { 'typ': 'text', 'value': selectedItem.get('value') });
+        
+        element.destroy();
     },
         
     toElement: function(){
